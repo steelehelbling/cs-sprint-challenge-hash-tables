38c5ac4f4a6e740e3cfa353b8a9ed1df16c7c64d
Steele Helbling <steelehelbling@gmail.com>
2020-09-04 11:12:15 -0400

    ver-1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,13 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    inside = {}
+    for index in range(length):
+        if weights[index] in inside:
+            return [index, inside[weights]]
 
     return None
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
+print(answer_4)
