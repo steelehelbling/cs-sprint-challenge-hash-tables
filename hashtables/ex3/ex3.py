8de727ca3e41ecf738b79fbe07ef66ad919121e4
Steele Helbling <steelehelbling@gmail.com>
2020-09-04 12:41:06 -0400

    ver-3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -6,11 +6,24 @@ def intersection(arrays):
     inside = {}
     for list_array in arrays:
         for array in list_array:
-           result = inside[array]
+            if array in inside:
+                inside[array] = inside[array] + 1
+            else:
+                inside[array] = 1
+    result = [item for item in inside.keys() if inside[item] == len(arrays)]
     return result
 
 
 if __name__ == "__main__":
+    result = intersection([
+                [1,2,3],
+                [1,4,5,3],
+                [1,6,7,3]
+    ])
+    print(result)
+    result.sort()
+    print(result) 
+
     arrays = []
 
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
